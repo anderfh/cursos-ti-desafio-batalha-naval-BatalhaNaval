27b764d3eee476b652f2d4bf4c4d9c9098300358
anderfh <farma.hunhoff@gmail.com>
2026-02-25 15:37:03 +0000

    Desafio Intermediário concluído

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,53 +1,93 @@
 #include <stdio.h>
 
+#define LINHAS 10 // Define tamanho do tabuleiro.
+#define COLUNAS 10
+
 // Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
 
 int main() {
     // Nível Novato - Posicionamento dos Navios
-    int tabuleiro [10] [10] = { // Matriz do tabuleiro.
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0}};
+    int tabuleiro [LINHAS] [COLUNAS]; // Matriz do tabuleiro.
+    for (int i = 0; i < LINHAS; i++){ // Loop define valores iniciais do tabuleiro.
+        for (int j = 0; j < COLUNAS; j++){
+            tabuleiro [i][j] = 0;
+        }        
+    }
     
-    char coluna [10] = {'A','B','C','D','E','F','G','H','I','J'}; // Legenda da coluna.
-    int linha [10] = {1,2,3,4,5,6,7,8,9,10}; // Legenda da linha.
-    int navio1v = 2, navio1h = 2; // Define posição inicial Navio 1.
-    int navio2v = 2, navio2h = 7; // Define posição inicial Navio 2.
+    char coluna [COLUNAS]; // Legenda da coluna.
+    for (int i = 0; i < COLUNAS; i++){ // Loop para valores da coluna.
+        coluna[i] = 'A' + i;
+    }
+    
+    int linha [LINHAS]; // Legenda da linha.
+    for (int i = 0; i < LINHAS; i++){ // Loop para valores da legenda.
+        linha[i] = i + 1;
+    }
+    
+    int navio1v = 0, navio1h = 7; // Define posição inicial Navio 1.
+    int navio2v = 2, navio2h = 4; // Define posição inicial Navio 2.
+    int navio3v = 5, navio3h = 2; // Define posição inicial Navio 3.
+    int navio4v = 4, navio4h = 8; // Define posição inicial Navio 4.
     char posiçãoNavio1x [3]; // Armazena posição eixo x Navio 1.
     int posiçãoNavio1y [3]; // Armazena posição eixo y Navio 1.
     char posiçãoNavio2x [3]; // Armazena posição eixo x Navio 2.
     int posiçãoNavio2y [3]; // Armazena posição eixo y Navio 2.
+    char posiçãoNavio3x [3]; // Armazena posição eixo x Navio 3.
+    int posiçãoNavio3y [3]; // Armazena posição eixo y Navio 3.
+    char posiçãoNavio4x [3]; // Armazena posição eixo x Navio 4.
+    int posiçãoNavio4y [3]; // Armazena posição eixo y Navio 4.
 
     for ( int v = navio1v; v < navio1v + 3; v++){ // Define posição Navio 1 - Vertical.
-        tabuleiro [v][navio1h] = 3;
+        tabuleiro [v][navio1h] = tabuleiro [v][navio1h] + 3;
         posiçãoNavio1x [v - navio1v] = coluna [navio1h];
         posiçãoNavio1y [v - navio1v] = linha [v];
     }
 
     for ( int h = navio2h; h < navio2h + 3; h++){ // Define posição Navio 2 - Horizontal.
-        tabuleiro [navio2v][h] = 3;
+        tabuleiro [navio2v][h] = tabuleiro [navio2v][h] + 3;
         posiçãoNavio2x [h - navio2h] = coluna [h];
         posiçãoNavio2y [h - navio2h] = linha [navio2v];
     }
 
-    if (navio1v < 0 || navio1v > 7 || navio1h < 0 || navio1h > 9){ // Verifica se Navio 1 está dentro dos limites do tabuleiro.
-        printf("O Navio 1 está fora dos limites do tabuleiro!");
-    }else if (navio2v < 0 || navio2v > 9 || navio2h < 0 || navio2h > 7){ // Verifica se Navio 2 está dentro dos limites do tabuleiro.
-        printf("O Navio 2 está fora dos limites do tabuleiro!");
-    }else if ((navio2v <= navio1v && navio2v >= navio1v-2) && (navio2h <= navio1h+2 && navio2h >= navio1h)){ // Verifica se Navio 2 está sobreposto a Navio 1.
-        printf("O Navio 2 está sobreposto com o Navio 1!");
+    for ( int v = navio3v; v < navio3v + 3; v++){ // Define posição Navio 3 - Diagonal decrescente.
+        int h = navio3h + (v - navio3v);
+        tabuleiro [v][h] = tabuleiro [v][h] + 3;
+        posiçãoNavio3x [v - navio3v] = coluna [h];
+        posiçãoNavio3y [v - navio3v] = linha [v];
+    }
+
+    for ( int v = navio4v; v < navio4v + 3; v++){ // Define posição Navio 4 - Diagonal crescente.
+        int h = navio4h - (v - navio4v);
+        tabuleiro [v][h] = tabuleiro [v][h] + 3;
+        posiçãoNavio4x [v - navio4v] = coluna [h];
+        posiçãoNavio4y [v - navio4v] = linha [v];
+    }
+
+    // Avaliando se navios estão dentro do tabuleiro corretamente.
+    int soma = 0; // Armazena a soma da matriz.
+    for (int i = 0; i < LINHAS; i++){
+        for (int j = 0; j < COLUNAS; j++){
+            soma += tabuleiro[i][j];
+        }
+    }
+
+    // Avaliando se há sobreposição de navios.
+    int somaMaior = 0; // Armazena soma de valores maiores de 3 na matriz.
+    for (int i = 0; i < LINHAS; i++){
+        for (int j = 0; j < COLUNAS; j++){
+            if(tabuleiro[i][j] > 3){
+                somaMaior = 1;
+            };
+        }
+    }
+
+    if (soma < 36){ // Aviso de Navio fora dos limites do tabuleiro.
+        printf("Algum navio está fora dos limites do tabuleiro!");
+    }else if (somaMaior == 1){ // Verifica se há sobreposição de navios.
+        printf("Há sobreposição de navios!");
     }else{
         printf ("As coordenadas do Navio 1 são: "); // Mostra as coordenadas do Navio 1.
-            for( int n = 0; n < 3; n++){ // Recursivo para mostrar as coordenadas de cada ponto.
+            for( int n = 0; n < 3; n++){ // Loop para mostrar as coordenadas de cada ponto.
                 printf ("%c%d", posiçãoNavio1x [n], posiçãoNavio1y [n]);
                 if (n < 2){ // Adiciona vírgula e espaço entre coordenadas.
                     printf (", ");
@@ -57,7 +97,7 @@ int main() {
             }
             printf("\n");
         printf ("As coordenadas do Navio 2 são: "); // Mostra as coordenadas do Navio 2.
-            for( int n = 0; n < 3; n++){ // Recursivo para mostrar as coordenadas de cada ponto.
+            for( int n = 0; n < 3; n++){ // Loop para mostrar as coordenadas de cada ponto.
                 printf ("%c%d", posiçãoNavio2x [n], posiçãoNavio2y [n]);
                 if (n < 2){ // Adiciona vírgula e espaço entre as coordenadas.
                     printf (", ");
@@ -66,11 +106,31 @@ int main() {
                 }
             }
             printf("\n");
+        printf ("As coordenadas do Navio 3 são: "); // Mostra as coordenadas do Navio 1.
+            for( int n = 0; n < 3; n++){ // Loop para mostrar as coordenadas de cada ponto.
+                printf ("%c%d", posiçãoNavio3x [n], posiçãoNavio3y [n]);
+                if (n < 2){ // Adiciona vírgula e espaço entre coordenadas.
+                    printf (", ");
+                }else{ // Adiciona ponto final ao fim das coordenadas.
+                    printf (".");
+                }
+            }
+            printf("\n");
+        printf ("As coordenadas do Navio 4 são: "); // Mostra as coordenadas do Navio 1.
+            for( int n = 0; n < 3; n++){ // Loop para mostrar as coordenadas de cada ponto.
+                printf ("%c%d", posiçãoNavio4x [n], posiçãoNavio4y [n]);
+                if (n < 2){ // Adiciona vírgula e espaço entre coordenadas.
+                    printf (", ");
+                }else{ // Adiciona ponto final ao fim das coordenadas.
+                    printf (".");
+                }
+            }
+            printf("\n");
         printf("\n - Tabuleiro Batalha Naval - \n\n"); // Exibe título do jogo.
         printf("    A B C D E F G H I J \n"); // Exibe a legenda da coluna.
-        for ( int i = 0; i < 10; i++){ // define a linha.
+        for ( int i = 0; i < LINHAS; i++){ // define a linha.
             printf("%2d- ", linha [i]); // Exibe a legenda da linha.
-            for(int j = 0; j < 10; j++){ // Define a coluna.
+            for(int j = 0; j < COLUNAS; j++){ // Define a coluna.
                 printf ( "%d " , tabuleiro [i][j]); // Exibe cada coordenada do tabuleiro.
             }
         printf("\n");
